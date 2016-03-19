044e0c80c11f6ae9e95ee2923c3494e630e09518
daniel <danielito@gmail.com>
2016-03-19 12:24:36 -0500

    added image, changed style

--- src/Index.js
+++ src/Index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 import App from './components/App';
-import PoweredBy from './components/Powered-by';
+import GameList from './components/GameList';
 import About from './components/About';
 
 window.React = React;
@@ -11,7 +11,7 @@ render(
   (<Router history={hashHistory}>
     <Route path="/" component={App}>
       <Route path="/about" component={About} />
-      <Route path="/poweredby" component={PoweredBy} />
+      <Route path="/game-list" component={PoweredBy} />
     </Route>
   </Router>), document.getElementById('content')
 );
---
+++ src/components/GameList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { dependencies, devDependencies } from '../../package.json';
+
+export default class PoweredBy extends React.Component {
+  render() {
+    const deps = Object.keys(dependencies)
+      .map((dep, i) => <li key={i}>{dep}</li>);
+    const devDeps = Object.keys(devDependencies)
+      .map((dep, i) => <li key={i + 10}>{dep}</li>);
+    return (
+      <div>
+        <h2>Powered by</h2>
+        <a href="https://david-dm.org/granze/react-starterify">
+          <img src="https://david-dm.org/granze/react-starterify/status.svg" alt="deps status" />
+        </a>
+        &nbsp;
+        <a href="https://david-dm.org/granze/react-starterify#info=devDependencies">
+          <img src="https://david-dm.org/granze/react-starterify/dev-status.svg" alt="dev deps status" />
+        </a>
+        <ul>
+          {[...deps, ...devDeps]}
+        </ul>
+      </div>
+    );
+  }
+}
