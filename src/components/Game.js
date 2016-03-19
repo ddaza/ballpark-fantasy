e5c5e2e77afd4645499b1a1fe4b50e4c5608bdcb
Tim Luo <tim@getclasskick.com>
2016-03-19 16:03:47 -0500

    wip get eventId in Game view

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,8 +6,8 @@ export default class Game extends React.Component {
   constructor(props) {
       super(props);
       let { query } = this.props.location;
-      this.state = {name: query.name,
-                    code: query.code};
+      this.state = { name: query.name,
+                     code: query.code };
   }
 
   componentWillMount() {
@@ -15,17 +15,25 @@ export default class Game extends React.Component {
       var sessionRef = new Firebase('https://ballparkfantasy.firebaseio.com/session').child(this.state.code);
       var userRef = sessionRef.child('users').child(this.state.name);
       userRef.once('value', function(dataSnapshot) {
+        // Write this new name if it doesn't already exist in this game.
         if (!dataSnapshot.exists()) {
           userRef.set(true);
         }
       });
+
+      var eventRef = sessionRef.child('eventId');
+      eventRef.once('value', function(dataSnapshot) {
+        if (dataSnapshot.exists()) {
+          this.setState({ event: dataSnapshot.val() });
+        }
+      }.bind(this));
     }
   }
 
   render() {
     //TODO: render the game
     return (
-      <p>game details here</p>
+      <p>{this.state.event}</p>
     );
   }
 }
