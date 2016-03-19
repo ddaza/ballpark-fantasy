c7c0e2da2681ab18ef134654bf4d015c1c3e4920
Tim Luo <tim@getclasskick.com>
2016-03-19 13:26:37 -0500

    add game code to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,16 +4,29 @@ export default class Login extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {name: ''};
+        this.state = {name: '',
+                      code: ''};
     }
 
     handleNameChange(e) {
         this.setState({name: e.target.value});
     }
 
+    handleCodeChange(e) {
+        this.setState({code: e.target.value});
+    }
+
     handleSubmit(e) {
         var name = this.state.name.trim();
-        window.location.hash = 'game_list?name=' + name;
+        var code = this.state.code.trim();
+        //TODO: verification of code, e.g. if code doesn't exist
+        //TODO: no distinction between "create" and "join" buttons, this will still
+        //try to log into code even if "create" was clicked
+        if (code) {
+            window.location.hash = 'game?name=' + name + '&code=' + code;
+        } else {
+            window.location.hash = 'game_list?name=' + name;
+        }
     }
 
     render() {
@@ -29,7 +42,12 @@ export default class Login extends React.Component {
                 />
                 <input type="submit" value="Create" />
                 <br/>
-                <input type="text" placeholder="Game Code" />
+                <input
+                  type="text"
+                  placeholder="Game Code"
+                  value={this.state.code}
+                  onChange={this.handleCodeChange.bind(this)}
+                />
                 <input type="submit" value="Join" />
             </form>
 
