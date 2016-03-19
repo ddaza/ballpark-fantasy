67094cadc5911ea0f606f81823b65e768b3d659e
daniel <danielito@gmail.com>
2016-03-19 16:02:33 -0500

    generate gamelist

--- src/api.js
+++ src/api.js
@@ -1,7 +1,10 @@
 import request from 'superagent';
 import {fromJS, Iterable} from 'immutable';
+import {sportRadar} from '../apikeys.json';
 
 const timeout = 5000;  // 5 seconds
+const corsBS = 'https://crossorigin.me/';
+const sportRadarPath = `${corsBS}http://api.sportradar.us/mlb-t5`;
 
 function handleCall(resolve, reject) {
   return function (err, res) {
@@ -15,6 +18,7 @@ function handleCall(resolve, reject) {
 }
 
 function handleImmutableQuery(query) {
+  console.log('querry!!!', query);
   if (Iterable.isIterable(query)) {
     return query.toJS();
   } else {
@@ -22,7 +26,7 @@ function handleImmutableQuery(query) {
   }
 }
 
-export function getToApi(path, query) {
+export function get(path, query) {
   return new Promise((resolve, reject) => {
     request
     .get(path)
@@ -32,7 +36,7 @@ export function getToApi(path, query) {
   });
 }
 
-export function postToApi(path, query) {
+export function post(path, query) {
   return new Promise((resolve, reject) => {
     request
     .post(path)
@@ -40,4 +44,12 @@ export function postToApi(path, query) {
     .timeout(timeout)
     .end(handleCall(resolve, reject));
   });
+}
+
+
+//http://api.sportradar.us/mlb-t5/games/2016/3/19/schedule.json?api_key=pcfhb22dmyvbphww2vkk6vd9
+
+export function getGamesSchedules(date) {
+  const path = `${sportRadarPath}/games/${date}/schedule.json?api_key=${sportRadar.apiKey}`;
+  return get(path);
 }
--- src/components/GameList.js
+++ src/components/GameList.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import {getGamesSchedules} from '../api';
+import moment from 'moment';
+import {Link} from 'react-router';
 
 export default class GameList extends React.Component {
+
+  constructor(context, props) {
+    super(context, props);
+    this.state = {games: '', code: ''};
+  }
+
+  componentDidMount() {
+    const today = moment().format('YYYY/MM/DD');
+    getGamesSchedules(today)
+    .then((result) => {
+      this.setState({games: result});
+    });
+  }
 
   goToPage(e) {
     const element = e.target;
@@ -12,13 +28,21 @@ export default class GameList extends React.Component {
   render() {
     return (
       <div>
-        <h2>HELLO</h2>
-        <input
-          type='button'
-          name='about'
-          value='About Page'
-          onClick={this.goToPage}
-        />
+        <h2>Games Today</h2>
+        { this.state.games ?
+          this.state.games.getIn(['league', 'games']).map((game) => {
+            return (
+              <div>
+                <Link to='/game?tobeadded=true'>
+                  <span>{game.getIn(['away', 'name'])}</span>
+                  <span> @ </span>
+                  <span>{game.getIn(['home', 'name'])},</span>
+                  <span> Status: {game.get('status')}</span>
+                </Link>
+              </div>
+            );
+          }) : null
+        }
       </div>
     );
   }
